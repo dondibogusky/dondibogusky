2b7aab8688cc58ea482a80a95d29010d23c6d0cd
Dondi Bogusky <hello@dondi.oib.com>
2018-04-04 22:49:15 -0700

    Rewrite medium-zoom-portfolio.js

--- source/js/medium-zoom-portfolio.js
+++ source/js/medium-zoom-portfolio.js
@@ -1,14 +1,19 @@
-const link01 = document.querySelector('#link01');
+const slider = document.querySelector('.slider');
+const link01a = document.querySelector('#link01');
+const zoom01a = document.querySelector('#zoom01');
 const zoom01 = mediumZoom('#zoom01', {
   background: 'rgba(0,0,0,0.75)',
   metaClick: false
 });
-link01.addEventListener('click', () => zoom01.show());
+
+link01a.addEventListener('click', () => zoom01.show(), {passive: true});
 $('#link01').click(function() {
   zoom01.show();
+  $('.slider').slick('slickPause');
 });
 $('#zoom01').click(function() {
   zoom01.show();
+  $('.slider').slick('slickPause');
 });
 
 const link02 = document.querySelector('#link02');
@@ -16,12 +21,14 @@ const zoom02 = mediumZoom('#zoom02', {
   background: 'rgba(0,0,0,0.75)',
   metaClick: false
 });
-link02.addEventListener('click', () => zoom02.show());
+link02.addEventListener('click', () => zoom02.show(), {passive: true});
 $('#link02').click(function() {
   zoom02.show();
+  $('.slider').slick('slickPause');
 });
 $('#zoom02').click(function() {
   zoom02.show();
+  $('.slider').slick('slickPause');
 });
 
 const link03 = document.querySelector('#link03');
@@ -29,10 +36,13 @@ const zoom03 = mediumZoom('#zoom03', {
   background: 'rgba(0,0,0,0.75)',
   metaClick: false
 });
-link03.addEventListener('click', () => zoom03.show());
+link03.addEventListener('click', () => zoom03.show(), {passive: true});
 $('#link03').click(function() {
   zoom03.show();
+  $('.slider').slick('slickPause');
 });
 $('#zoom03').click(function() {
   zoom03.show();
+  $('.slider').slick('slickPause');
 });
+
